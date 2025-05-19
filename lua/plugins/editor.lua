0cb24d054935a57269cbbbf56ee9027def9aae15
Emtyffx <p.verbytsky@gmail.com>
2025-05-19 10:07:07 +0300

    nvim-ts-autotag

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -58,4 +58,14 @@ return {
 		"echasnovski/mini.surround",
 		opts = {},
 	},
+	{
+		"windwp/nvim-ts-autotag",
+		opts = {
+			opts = {
+				enable_close = true,
+				enable_rename = true,
+				enable_close_on_slash = true,
+			},
+		},
+	},
 }
